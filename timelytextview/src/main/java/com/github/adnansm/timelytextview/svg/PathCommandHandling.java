2b2c0be1aa68266261253bc709c49fa65c488d35
Scotty Tomaszewski <scottTomaszewski@gmail.com>
2014-10-14 21:19:47 -0400

    Adds support for S and T commands

--- timelytextview/src/main/java/com/github/adnansm/timelytextview/svg/PathCommandHandling.java
+++ timelytextview/src/main/java/com/github/adnansm/timelytextview/svg/PathCommandHandling.java
@@ -1,10 +1,13 @@
 package com.github.adnansm.timelytextview.svg;
 
+import android.graphics.Point;
+
 public abstract class PathCommandHandling {
     private int lastX;
     private int lastY;
     private int firstX;
     private int firstY;
+    private Point lastControlPoint;
 
     protected final int lastX() {
         return lastX;
@@ -23,15 +26,15 @@ public abstract class PathCommandHandling {
     }
 
     protected final int nextControlX() {
-        return 0;
+        return lastX() + (lastX() - lastControlPoint.x);
     }
 
     protected final int nextControlY() {
-        return 0;
+        return lastY() + (lastY() - lastControlPoint.y);
     }
 
     final void process_M(int endX, int endY) {
-        handle_M(endX, endY);
+        lastControlPoint = handle_M(endX, endY);
         lastX = endX;
         lastY = endY;
         firstX = endX;
@@ -39,131 +42,131 @@ public abstract class PathCommandHandling {
     }
 
     final void process_m(int d_endX, int d_endY) {
-        handle_m(d_endX, d_endY);
+        lastControlPoint = handle_m(d_endX, d_endY);
         lastX = lastX + d_endX;
         lastY = lastY + d_endY;
     }
 
     final void process_L(int endX, int endY) {
-        handle_L(endX, endY);
+        lastControlPoint = handle_L(endX, endY);
         lastX = endX;
         lastY = endY;
     }
 
     final void process_l(int d_endX, int d_endY) {
-        handle_l(d_endX, d_endY);
+        lastControlPoint = handle_l(d_endX, d_endY);
         lastX = lastX + d_endX;
         lastY = lastY + d_endY;
     }
 
     final void process_H(int endX) {
-        handle_H(endX);
+        lastControlPoint = handle_H(endX);
         lastX = endX;
     }
 
     final void process_h(int d_endX) {
-        handle_h(d_endX);
+        lastControlPoint = handle_h(d_endX);
         lastX = lastX + d_endX;
     }
 
     final void process_V(int endY) {
-        handle_V(endY);
+        lastControlPoint = handle_V(endY);
         lastY = endY;
     }
 
     final void process_v(int d_endY) {
-        handle_v(d_endY);
+        lastControlPoint = handle_v(d_endY);
         lastY = lastY + d_endY;
     }
 
     final void process_Z() {
-        handle_Z();
+        lastControlPoint = handle_Z();
         lastX = firstX;
         lastY = firstY;
     }
 
     final void process_C(int control1X, int control1Y, int control2X, int control2Y, int endX, int endY) {
-        handle_C(control1X, control1Y, control2X, control2Y, endX, endY);
+        lastControlPoint = handle_C(control1X, control1Y, control2X, control2Y, endX, endY);
         lastX = endX;
         lastY = endY;
     }
 
     final void process_c(int d_control1X, int d_control1Y, int d_control2X, int d_control2Y, int d_endX, int d_endY) {
-        handle_c(d_control1X, d_control1Y, d_control2X, d_control2Y, d_endX, d_endY);
+        lastControlPoint = handle_c(d_control1X, d_control1Y, d_control2X, d_control2Y, d_endX, d_endY);
         lastX = lastX + d_endX;
         lastY = lastY + d_endY;
     }
 
     final void process_S(int control2X, int control2Y, int endX, int endY) {
         // TODO: check that this is legal
-        handle_S(control2X, control2Y, endX, endY);
+        lastControlPoint = handle_S(control2X, control2Y, endX, endY);
         lastX = endX;
         lastY = endY;
     }
 
     final void process_s(int d_control2X, int d_control2Y, int d_endX, int d_endY) {
-        handle_s(d_control2X, d_control2Y, d_endX, d_endY);
+        lastControlPoint = handle_s(d_control2X, d_control2Y, d_endX, d_endY);
         lastX = lastX + d_endX;
         lastY = lastY + d_endY;
     }
 
     final void process_Q(int controlX, int controlY, int endX, int endY) {
-        handle_Q(controlX, controlY, endX, endY);
+        lastControlPoint = handle_Q(controlX, controlY, endX, endY);
         lastX = endX;
         lastY = endY;
     }
 
     final void process_q(int d_controlX, int d_controlY, int d_endX, int d_endY) {
-        handle_q(d_controlX, d_controlY, d_endX, d_endY);
+        lastControlPoint = handle_q(d_controlX, d_controlY, d_endX, d_endY);
         lastX = lastX + d_endX;
         lastY = lastY + d_endY;
     }
 
     final void process_T(int endX, int endY) {
-        handle_T(endX, endY);
+        lastControlPoint = handle_T(endX, endY);
         lastX = endX;
         lastY = endY;
     }
 
     final void process_t(int d_endX, int d_endY) {
-        handle_t(d_endX, d_endY);
+        lastControlPoint = handle_t(d_endX, d_endY);
         lastX = lastX + d_endX;
         lastY = lastY + d_endY;
     }
 
     // implementations
 
-    protected abstract void handle_M(int endX, int endY);
+    protected abstract Point handle_M(int endX, int endY);
 
-    protected abstract void handle_m(int d_endX, int d_endY);
+    protected abstract Point handle_m(int d_endX, int d_endY);
 
-    protected abstract void handle_L(int endX, int endY);
+    protected abstract Point handle_L(int endX, int endY);
 
-    protected abstract void handle_l(int d_endX, int d_endY);
+    protected abstract Point handle_l(int d_endX, int d_endY);
 
-    protected abstract void handle_H(int endX);
+    protected abstract Point handle_H(int endX);
 
-    protected abstract void handle_h(int d_endX);
+    protected abstract Point handle_h(int d_endX);
 
-    protected abstract void handle_V(int endY);
+    protected abstract Point handle_V(int endY);
 
-    protected abstract void handle_v(int d_endY);
+    protected abstract Point handle_v(int d_endY);
 
-    protected abstract void handle_Z();
+    protected abstract Point handle_Z();
 
-    protected abstract void handle_C(int control1X, int control1Y, int control2x, int control2Y, int endX, int endY);
+    protected abstract Point handle_C(int control1X, int control1Y, int control2x, int control2Y, int endX, int endY);
 
-    protected abstract void handle_c(int d_control1X, int d_control1Y, int d_control2x, int d_control2Y, int d_endX, int d_endY);
+    protected abstract Point handle_c(int d_control1X, int d_control1Y, int d_control2x, int d_control2Y, int d_endX, int d_endY);
 
-    protected abstract void handle_S(int control2x, int control2Y, int endX, int endY);
+    protected abstract Point handle_S(int control2x, int control2Y, int endX, int endY);
 
-    protected abstract void handle_s(int d_control2x, int d_control2Y, int d_endX, int d_endY);
+    protected abstract Point handle_s(int d_control2x, int d_control2Y, int d_endX, int d_endY);
 
-    protected abstract void handle_Q(int controlX, int controlY, int endX, int endY);
+    protected abstract Point handle_Q(int controlX, int controlY, int endX, int endY);
 
-    protected abstract void handle_q(int d_controlX, int d_controlY, int d_endX, int d_endY);
+    protected abstract Point handle_q(int d_controlX, int d_controlY, int d_endX, int d_endY);
 
-    protected abstract void handle_T(int endX, int endY);
+    protected abstract Point handle_T(int endX, int endY);
 
-    protected abstract void handle_t(int d_endX, int d_endY);
+    protected abstract Point handle_t(int d_endX, int d_endY);
 }
--- timelytextview/src/main/java/com/github/adnansm/timelytextview/svg/PathCommandToCubicHandling.java
+++ timelytextview/src/main/java/com/github/adnansm/timelytextview/svg/PathCommandToCubicHandling.java
@@ -1,5 +1,7 @@
 package com.github.adnansm.timelytextview.svg;
 
+import android.graphics.Point;
+
 import com.github.adnansm.timelytextview.model.Cubic;
 import com.google.common.collect.Lists;
 
@@ -9,105 +11,120 @@ public class PathCommandToCubicHandling extends PathCommandHandling {
     private final List<Cubic> all = Lists.newArrayList();
 
     @Override
-    protected void handle_M(int endX, int endY) {
-        // do nothing
+    protected Point handle_M(int endX, int endY) {
+        return new Point(endX, endY);
     }
 
     @Override
-    protected void handle_m(int d_endX, int d_endY) {
-        // do nothing
+    protected Point handle_m(int d_endX, int d_endY) {
+        return new Point(lastX() + d_endX, lastY() + d_endY);
     }
 
     @Override
-    protected void handle_L(int endX, int endY) {
+    protected Point handle_L(int endX, int endY) {
         all.add(new Cubic(lastX(), lastY(), lastX(), lastY(),
                 endX, endY, endX, endY));
+        return new Point(endX, endY);
     }
 
     @Override
-    protected void handle_l(int d_endX, int d_endY) {
+    protected Point handle_l(int d_endX, int d_endY) {
         all.add(new Cubic(lastX(), lastY(), lastX(), lastY(),
                 lastX() + d_endX, lastY() + d_endY, lastX() + d_endX, lastY() + d_endY));
+        return new Point(lastX() + d_endX, lastY() + d_endY);
     }
 
     @Override
-    protected void handle_H(int endX) {
+    protected Point handle_H(int endX) {
         all.add(new Cubic(lastX(), lastY(), lastX(), lastY(),
                 endX, lastY(), endX, lastY()));
+        return new Point(endX, lastY());
     }
 
     @Override
-    protected void handle_h(int d_endX) {
+    protected Point handle_h(int d_endX) {
         all.add(new Cubic(lastX(), lastY(), lastX(), lastY(),
                 lastX() + d_endX, lastY(), lastX() + d_endX, lastY()));
+        return new Point(lastX() + d_endX, lastY());
     }
 
     @Override
-    protected void handle_V(int endY) {
+    protected Point handle_V(int endY) {
         all.add(new Cubic(lastX(), lastY(), lastX(), lastY(),
                 lastX(), endY, lastX(), endY));
+        return new Point(lastX(), endY);
     }
 
     @Override
-    protected void handle_v(int d_endY) {
+    protected Point handle_v(int d_endY) {
         all.add(new Cubic(lastX(), lastY(), lastX(), lastY(),
                 lastX(), lastY() + d_endY, lastX(), lastY() + d_endY));
+        return new Point(lastX(), lastY() + d_endY);
     }
 
     @Override
-    protected void handle_Z() {
+    protected Point handle_Z() {
         all.add(new Cubic(lastX(), lastY(), lastX(), lastY(),
                 firstX(), firstY(), firstX(), firstY()));
+        return new Point(firstX(), firstY());
     }
 
     @Override
-    protected void handle_C(int control1X, int control1Y, int control2x, int control2Y, int endX, int endY) {
+    protected Point handle_C(int control1X, int control1Y, int control2x, int control2Y, int endX, int endY) {
         all.add(new Cubic(lastX(), lastY(), control1X, control1Y,
                 control2x, control2Y, endX, endY));
+        return new Point(control2x, control2Y);
     }
 
     @Override
-    protected void handle_c(int d_control1X, int d_control1Y, int d_control2x, int d_control2Y, int d_endX, int d_endY) {
+    protected Point handle_c(int d_control1X, int d_control1Y, int d_control2x, int d_control2Y, int d_endX, int d_endY) {
         all.add(new Cubic(lastX(), lastY(),
                 lastX() + d_control1X, lastY() + d_control1Y,
                 lastX() + d_control2x, lastY() + d_control2Y,
                 lastX() + d_endX, lastY() + d_endY));
+        return new Point(lastX() + d_control2x, lastY() + d_control2Y);
     }
 
     @Override
-    protected void handle_S(int control2x, int control2Y, int endX, int endY) {
+    protected Point handle_S(int control2x, int control2Y, int endX, int endY) {
         all.add(new Cubic(lastX(), lastY(), nextControlX(), nextControlY(),
                 control2x, control2Y, endX, endY));
+        return new Point(control2x, control2Y);
     }
 
     @Override
-    protected void handle_s(int d_control2x, int d_control2Y, int d_endX, int d_endY) {
+    protected Point handle_s(int d_control2x, int d_control2Y, int d_endX, int d_endY) {
         all.add(new Cubic(lastX(), lastY(), nextControlX(), nextControlY(),
                 lastX() + d_control2x, lastY() + d_control2Y, lastX() + d_endX, lastY() + d_endY));
+        return new Point(lastX() + d_control2x, lastY() + d_control2Y);
     }
 
     @Override
-    protected void handle_Q(int controlX, int controlY, int endX, int endY) {
+    protected Point handle_Q(int controlX, int controlY, int endX, int endY) {
         all.add(new Cubic(lastX(), lastY(), controlX, controlY,
                 controlX, controlY, endX, endY));
+        return new Point(controlX, controlY);
     }
 
     @Override
-    protected void handle_q(int d_controlX, int d_controlY, int d_endX, int d_endY) {
+    protected Point handle_q(int d_controlX, int d_controlY, int d_endX, int d_endY) {
         all.add(new Cubic(lastX(), lastY(), lastX() + d_controlX, lastY() + d_controlY,
                 lastX() + d_controlX, lastY() + d_controlY, lastX() + d_endX, lastY() + d_endY));
+        return new Point(lastX() + d_controlX, lastY() + d_controlY);
     }
 
     @Override
-    protected void handle_T(int endX, int endY) {
+    protected Point handle_T(int endX, int endY) {
         all.add(new Cubic(lastX(), lastY(), nextControlX(), nextControlY(),
                 nextControlX(), nextControlY(), endX, endY));
+        return new Point(nextControlX(), nextControlY());
     }
 
     @Override
-    protected void handle_t(int d_endX, int d_endY) {
+    protected Point handle_t(int d_endX, int d_endY) {
         all.add(new Cubic(lastX(), lastY(), nextControlX(), nextControlY(),
                 nextControlX(), nextControlY(), lastX() + d_endX, lastY() + d_endY));
+        return new Point(nextControlX(), nextControlY());
     }
 
     public List<Cubic> getPathAsCubics() {
