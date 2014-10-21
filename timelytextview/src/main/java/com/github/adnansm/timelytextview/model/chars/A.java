d3c3bfce0bc5357a9f396bd2918b74efdfcacbd7
Scotty Tomaszewski <scottTomaszewski@gmail.com>
2014-10-20 21:53:17 -0400

    Replaces 'D' with 'E' for demo

--- timelytextview/src/main/java/com/github/adnansm/timelytextview/model/chars/A.java
+++ timelytextview/src/main/java/com/github/adnansm/timelytextview/model/chars/A.java
@@ -7,7 +7,9 @@ public class A {
 //    private static final String ORIGINAL = "M207 0v1462h395C952 1462 952 1462 1134.5 1279C1317 1096 1317 1096 1317 745z";
 //    private static final String ORIGINAL = "M207 0v1462h395q350 0 532.5 -183t182.5 -534z";
     // D
-    private static final String ORIGINAL = "M1317 745q0 -368 -193 -556.5t-567 -188.5h-350v1462h395q350 0 532.5 -183t182.5 -534zM1206 741q0 314 -159.5 472.5t-468.5 158.5h-269v-1282h242q655 0 655 651z";
+//    private static final String ORIGINAL = "M1317 745q0 -368 -193 -556.5t-567 -188.5h-350v1462h395q350 0 532.5 -183t182.5 -534zM1206 741q0 314 -159.5 472.5t-468.5 158.5h-269v-1282h242q655 0 655 651z";
+    // E
+    private static final String ORIGINAL = "M207 0v1462h799v-94h-697v-553h658v-94h-658v-627h697v-94h-799z";
 
     // A
 //    private static final String ORIGINAL = "M0 0l588 1468h65l576 -1468h-115l-203 516h-594l-204 -516h-113zM354 608h523l-199 527q-25 62 -60 172q-27 -96 -59 -174z";
